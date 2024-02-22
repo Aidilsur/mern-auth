e614aa663bde990be4bc71c52888643e5650392e
aidil <aidil.surya@mandalafinance.com>
2024-02-22 19:35:19 +0700

    private route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,9 +16,19 @@ const Header = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="sign-in">Sign In</Link>
-          </li>
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            </Link>
+          ) : (
+            <li>
+              <Link to="sign-in">Sign In</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
